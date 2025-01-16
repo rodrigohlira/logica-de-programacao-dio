c457c1934911b86b363e361a3155d9f17f166724
rodrigohlira <rodrigohlira@outlook.com>
2025-01-16 13:19:28 -0400

    Desafio de código Dio.me

---
+++ Desafio-Dio.me/classificador-nivel-heroi.js
@@ -0,0 +1,28 @@
+//Digite o nome do herói
+
+let nomeHeroi = "Rodrigo"
+
+//Digite a quantidade de experiência/XP
+
+let xp = 6800;
+let nivel = "";
+
+if (xp <= 1000){
+    nivel = "Ferro"
+} else if (xp > 1000 && xp <= 2000){
+    nivel = "Bronze"
+} else if (xp > 2000 && xp <= 5000){
+    nivel = "Prata"
+} else if (xp > 5000 && xp <= 7000){
+    nivel = "Ouro"
+} else if (xp > 7000 && xp <= 8000){
+    nivel = "Platina"
+} else if (xp > 8000 && xp <= 9000){
+    nivel = "Ascendente"
+} else if (xp > 9000 && xp <= 10000){
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
+}
+
+console.log(`O herói de nome ${nomeHeroi} está no nível ${nivel}`)
